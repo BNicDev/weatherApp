27db96988033620cc1f0313a3fbd2b8b0c156088
Nico Pizarro <bnicdev@gmail.com>
2025-11-06 22:02:46 -0300

    avances

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,49 @@
-let API_KEY = '1ea4f89accf91abda486b4e66914993b'
-let API = `https://api.openweathermap.org/data/2.5/weather?q=ezeiza&appid=${API_KEY}`
+let API_KEY = '1ea4f89accf91abda486b4e66914993b';
+let searchData = ''
+let buscar = document.getElementById('search');
+let informacionFormulario = document.getElementById('formularioData')
+let DATA = []
 
-async function DATA_API(){
+const dayElemento = document.getElementById('date');
+const opcionesFecha = { 
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric' 
+};
+
+// Formatear la fecha actual en español
+dayElemento.innerText = new Date().toLocaleDateString('es-ES', opcionesFecha);
+
+informacionFormulario.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const inputData = document.getElementById('input');
+    searchData = inputData.value.trim();
+    if(searchData){
+        DATA_API(searchData, API_KEY)
+    }else{
+        alert('ingrese una ciudad')
+    }
+    console.log(DATA)
+})
+
+async function DATA_API(s,ak){
+
+    let API = `https://api.openweathermap.org/data/2.5/weather?q=${s}&appid=${ak}`;
+
     try{
         const response = await fetch(API);
         const data = await response.json();
 
-        console.log(data)
+        DATA.length = 0;
+        DATA.push(data);
+        // console.log(data)
     }catch(e){
         console.log('hay un error ', e)
     }
 }
-DATA_API()
+
+function mostrarClima(datos){
+
+}
+mostrarClima()
